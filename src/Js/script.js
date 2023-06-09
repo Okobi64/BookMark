7454e0ebfb1989c86a7b6a070e5498e05865ee67
unknown <Okobi64@users.noreply.github.com>
2023-06-09 08:25:28 +0100

    initial commit

---
+++ src/Js/script.js
@@ -0,0 +1,26 @@
+const tabs = document.querySelectorAll(".tab");
+const panels = document.querySelectorAll(".panel");
+
+// Tabs event listener
+tabs.forEach((tab) => tab.addEventListener("click", onTabClick));
+
+function onTabClick(e) {
+  tabs.forEach((tab) => {
+    tab.children[0].classList.remove(
+      "border-softRed",
+      "border-b-4",
+      "md:border-b-0"
+    );
+  });
+
+  // Hide all panels
+  panels.forEach((panel) => panel.classList.add("hidden"));
+
+  // New tab and panel based on the target tab
+  e.target.classList.add("border-softRed", "border-b-4");
+  const classString = e.target.getAttribute("data-target");
+  document
+    .getElementById("panels")
+    .getElementsByClassName("classString")[0]
+    .classList.remove("hidden");
+}

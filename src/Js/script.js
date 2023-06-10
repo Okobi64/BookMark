f6f118e9c14af9194bd08f4889b18f9437f88f59
unknown <Okobi64@users.noreply.github.com>
2023-06-10 11:30:23 +0100

    initial commit

--- src/Js/script.js
+++ src/Js/script.js
@@ -6,11 +6,7 @@ tabs.forEach((tab) => tab.addEventListener("click", onTabClick));
 
 function onTabClick(e) {
   tabs.forEach((tab) => {
-    tab.children[0].classList.remove(
-      "border-softRed",
-      "border-b-4",
-      "md:border-b-0"
-    );
+    tab.classList.remove("border-softRed", "border-b-4", "md:border-b-0");
   });
 
   // Hide all panels
@@ -18,9 +14,13 @@ function onTabClick(e) {
 
   // New tab and panel based on the target tab
   e.target.classList.add("border-softRed", "border-b-4");
-  const classString = e.target.getAttribute("data-target");
-  document
-    .getElementById("panels")
-    .getElementsByClassName("classString")[0]
-    .classList.remove("hidden");
+
+  // get target from data attribute
+  const target = e.target.dataset.target;
+
+  const targetPanel = document.querySelector(`.${target}`);
+
+  if (targetPanel) {
+    targetPanel.classList.remove("hidden");
+  }
 }
